b2d56918115fbd72dc696da130e31cfb770ec9b7
MayankYadav022 <ydvmayank3@gmail.com>
2026-04-03 02:39:36 +0530

    resolved server error

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const path = require('path');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
 
-app.use(express.static('build'));
-app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/health', (req, res) => {
+    res.status(200).send('ok');
 });
 
 
